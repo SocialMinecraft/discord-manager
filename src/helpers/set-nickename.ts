2eda6591e8553b6c0fab0e6678d62c8efb2e96b9
Mazey-Jessica "Clove" Emily Twilight <alextlm@mazeymoos.com>
2025-01-24 02:40:42 +0000

    Update set-nickename.ts
    Eliminate whitespace issues that may cause an error

--- src/helpers/set-nickename.ts
+++ src/helpers/set-nickename.ts
@@ -1,37 +1,45 @@
-import {REST, Routes} from "discord.js";
-import {config} from "../config";
+import { REST, Routes } from "discord.js";
+import { config } from "../config";
 import * as validators from "./validators";
 
 const rest = new REST({ version: "10" }).setToken(config.DISCORD_TOKEN);
 
-// Will only set the nickname if a valid one is not present. (unless force=true)
 export async function setNickname(
-    guild_id: string, member_id: string, current_nick: string|null,
-    first_name: string, mc_name: string ) {
+    guild_id: string,
+    member_id: string,
+    current_nick: string | null,
+    first_name: string,
+    mc_name: string
+) {
+    // Trim the first_name to remove any extra whitespace
+    const trimmedFirstName = first_name?.trim();
 
-    // do they have a valid discord name already and this is not a force update?
+    // Log to verify the trimmed name
+    console.log('Trimmed first_name:', JSON.stringify(trimmedFirstName));
+
+    // Check if the user already has a valid Discord nickname and this is not a forced update
     if (current_nick != null && validators.isValidDiscordName(current_nick)) {
         return "";
     }
 
-    // check first name
-    if (!validators.isValidFirstName(first_name)) {
+    // Validate the trimmed first name
+    if (!validators.isValidFirstName(trimmedFirstName)) {
         return 'Please double check your first name and then try again. It needs to be at least 3 characters and no more than 12 characters.';
     }
 
     // Build NickName
-    const nick = first_name + ' (' + mc_name + ')';
+    const nick = `${trimmedFirstName} (${mc_name})`;
 
     // Update the name
     try {
         await rest.patch(Routes.guildMember(guild_id, member_id), {
             body: {
                 nick: nick,
-            }
-        })
+            },
+        });
     } catch (err) {
         console.log(err);
-        //return "An Error occured while attempting to update your discord name."
+        // Optionally return an error message if needed
     }
 
     return "";
