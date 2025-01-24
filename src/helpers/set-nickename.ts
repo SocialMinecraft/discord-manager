bef1b8cd9ebd7b40be15b1dbfc2820dd9d9dcf02
Mazey-Jessica "Clove" Emily Twilight <alextlm@mazeymoos.com>
2025-01-24 02:39:14 +0000

    Update set-nickename.ts
    Wrong file

--- src/helpers/set-nickename.ts
+++ src/helpers/set-nickename.ts
@@ -5,41 +5,34 @@ import * as validators from "./validators";
 const rest = new REST({ version: "10" }).setToken(config.DISCORD_TOKEN);
 
 // Will only set the nickname if a valid one is not present. (unless force=true)
-export async function execute(interaction: CommandInteraction) {
-    const uid = interaction.member!.user.id;
-    // @ts-ignore
-    const first_name = interaction.options.getString('first_name');
-    // @ts-ignore
-    const mc_username = interaction.options.getString('minecraft_name');
+export async function setNickname(
+    guild_id: string, member_id: string, current_nick: string|null,
+    first_name: string, mc_name: string ) {
 
-    // Trim the first name to remove any extra whitespace, should fix any issues
-    const trimmedName = first_name?.trim();
-
-    console.log('Extracted and trimmed first_name:', JSON.stringify(trimmedName));
-    console.log('Extracted mc_username:', mc_username);
-
-    // Set the user's nickname (if needed).
-    // @ts-ignore
-    let err = await setNickname(interaction.guildId!, uid, interaction.member!.nickname, trimmedName, mc_username);
-    if (err.length > 0) {
-        return interaction.reply({
-            content: err,
-            ephemeral: true,
-        });
+    // do they have a valid discord name already and this is not a force update?
+    if (current_nick != null && validators.isValidDiscordName(current_nick)) {
+        return "";
     }
 
-    // Add the account
-    err = await addMinecraftAccount(uid, trimmedName, mc_username);
-    if (err.length > 0) {
-        return interaction.reply({
-            content: err,
-            ephemeral: true,
-        });
+    // check first name
+    if (!validators.isValidFirstName(first_name)) {
+        return 'Please double check your first name and then try again. It needs to be at least 3 characters and no more than 12 characters.';
     }
 
-    // Success!
-    return interaction.reply({
-        content: "Success! Your account has been registered.",
-        ephemeral: true,
-    });
+    // Build NickName
+    const nick = first_name + ' (' + mc_name + ')';
+
+    // Update the name
+    try {
+        await rest.patch(Routes.guildMember(guild_id, member_id), {
+            body: {
+                nick: nick,
+            }
+        })
+    } catch (err) {
+        console.log(err);
+        //return "An Error occured while attempting to update your discord name."
+    }
+
+    return "";
 }
