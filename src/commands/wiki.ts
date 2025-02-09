1d95e078668b96336bad482a23c0b0cdc7d0f24c
Clove Twilight <alextlm@mazeymoos.com>
2025-02-09 20:52:21 +0000

    Create wiki.ts
    Create a wiki command with subsections to the wiki, very flexible and can be added onto

---
+++ src/commands/wiki.ts
@@ -0,0 +1,35 @@
+import { ChatInputCommandInteraction, SlashCommandBuilder } from 'discord.js';
+
+const WIKI_LINK = 'https://doc.clickup.com/9013455369/d/h/8ckwug9-53/856b548acfcac7e';
+const SECTIONS: Record<string, string> = {
+    'vip': 'https://doc.clickup.com/9013455369/d/h/8ckwug9-53/856b548acfcac7e/8ckwug9-673',
+    'game-modes': 'https://doc.clickup.com/9013455369/d/h/8ckwug9-53/856b548acfcac7e/8ckwug9-533',
+    'general-how-tos': 'https://doc.clickup.com/9013455369/d/h/8ckwug9-53/856b548acfcac7e/8ckwug9-373',
+    'season-3-government': 'https://doc.clickup.com/9013455369/d/h/8ckwug9-53/856b548acfcac7e/8ckwug9-73'
+};
+
+export const data = new SlashCommandBuilder()
+    .setName('wiki')
+    .setDescription('Get the link to the official wiki.')
+    .addStringOption(option => 
+        option.setName('section')
+            .setDescription('Specify a section of the wiki to visit')
+            .setRequired(false)
+            .addChoices(
+                { name: 'VIP', value: 'vip' },
+                { name: 'Game Modes', value: 'game-modes' },
+                { name: 'General How-Tos', value: 'general-how-tos' },
+                { name: 'Season 3 Government', value: 'season-3-government' }
+            )
+    );
+
+export async function execute(interaction: ChatInputCommandInteraction) {
+    const section = interaction.options.getString('section');
+    if (!section) {
+        await interaction.reply({ content: `📖 Here is the main wiki link: <${WIKI_LINK}>`, ephemeral: true });
+        return;
+    }
+    
+    const link = SECTIONS[section] || WIKI_LINK;
+    await interaction.reply({ content: `📖 Here is the ${section.replace(/-/g, ' ')} wiki link: <${link}>`, ephemeral: true });
+}
